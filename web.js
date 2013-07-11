9ab304da9e8e2799e210f33cb7dcf703b96b9365
Ajan Lal Shrestha <ajan_reddevil@hotmail.com>
2013-07-11 03:52:41 +0000

    Fied the bracket error

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync('index.html');
+  console.log(buffer.toString('utf-8'));
   response.send(buffer.toString('utf-8');
 });
 
